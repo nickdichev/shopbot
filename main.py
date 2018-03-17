7edb82f704f7d942cec5fc30a5ac7e2bc4e9b08b
Nick Dichev <nickdichev@yahoo.com>
2018-03-17 00:30:57 -0700

    cleaned up logic in main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def fail_and_exit(err_str):
 parser = argparse.ArgumentParser(description='Shopbot automates your reddit shopping experience!')
 parser.add_argument('--pm', action='store_true', help='Notify via Reddit pm (default: notify via Twilio)')
 args = parser.parse_args()
+should_pm = args.pm
 
 if config.client_id == '' or config.client_id is None:
 	fail_and_exit("client_id configuration is empty")
@@ -26,9 +27,9 @@ if config.user_agent == '' or config.user_agent is None:
 	fail_and_exit("user_agent configuration is empty")
 
 if config.bitly_token == '' or config.bitly_token is None:
-	args.pm = True
+	print 'Bit.ly token not provided! Long URLs will be included in output'
 
-if not args.pm:
+if not should_pm:
 	if config.account_sid == '' or config.account_sid is None:
 		fail_and_exit("account_sid configuration is empty")
 	if config.auth_token == '' or config.auth_token is None:
@@ -45,5 +46,5 @@ reddit = praw.Reddit(client_id = config.client_id,
 		     user_agent = config.user_agent)
 
 shopbot.parse(reddit)
-notify.notify(args.pm, reddit)
+notify.notify(should_pm, reddit)
 os.remove('found.json')
