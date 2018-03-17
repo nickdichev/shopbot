1a2619183907be0622ddc12885d5fde3859124ca
Nick Dichev <nickdichev@yahoo.com>
2018-03-16 23:00:16 -0700

    don't shorten urls if bitly_token is not provided

--- bitly.py
+++ bitly.py
@@ -4,8 +4,8 @@ import urllib
 import re
 
 def get_short_url(long_url):
-	# don't shorten bit.ly URLs or we will get a INVALID_URI response
-	if re.match('https?://bit.ly', long_url):
+	# don't shorten bit.ly URLs or if config doens't have a token
+	if re.match('https?://bit.ly', long_url) or config.bitly_token == '' or config.bitly_token is None:
 		return long_url 
 	encoded_url = urllib.quote(long_url, safe='')
 	r = requests.get('https://api-ssl.bitly.com/v3/shorten?access_token={}&longUrl={}&format=txt'.format(config.bitly_token, encoded_url))
